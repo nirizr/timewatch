f2b9803d9d06c30a0f32651e36d19053b6873dcd
Nir Izraeli <nir@sentinel-labs.com>
2016-07-19 17:24:44 +0300

    Automatic requirements based on requirements.txt and version bump
    Signed-off-by: Nir Izraeli <nir@sentinel-labs.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from distutils.core import setup
+
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
 setup(
   name = 'timewatch',
   packages = ['timewatch'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.4',
   description = 'A library automating worktime reports for timewatch.co.il',
   author = 'Nir Izraeli',
   author_email = 'nirizr@gmail.com',
   url = 'https://github.com/nirizr/timewatch', # use the URL to the github repo
-  download_url = 'https://github.com/nirizr/timewatch/tarball/0.3', # I'll explain this in a second
+  download_url = 'https://github.com/nirizr/timewatch/tarball/0.4', # I'll explain this in a second
   keywords = ['timewatch', 'timewatch.co.il'], # arbitrary keywords
   classifiers = [],
+  install_requires = requirements
 )
