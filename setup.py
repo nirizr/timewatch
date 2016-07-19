f30c9821d666c105074596a66e9f407f6f9dca31
Nir Izraeli <nir@sentinel-labs.com>
2016-07-19 15:26:30 +0300

    bump version
    Signed-off-by: Nir Izraeli <nir@sentinel-labs.com>

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'timewatch',
   packages = ['timewatch'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'A library automating worktime reports for timewatch.co.il',
   author = 'Nir Izraeli',
   author_email = 'nirizr@gmail.com',
   url = 'https://github.com/nirizr/timewatch', # use the URL to the github repo
-  download_url = 'https://github.com/nirizr/timewatch/tarball/0.2', # I'll explain this in a second
+  download_url = 'https://github.com/nirizr/timewatch/tarball/0.3', # I'll explain this in a second
   keywords = ['timewatch', 'timewatch.co.il'], # arbitrary keywords
   classifiers = [],
 )
