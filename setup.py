04156dffae6ed65eb72a26304cec276ec9130c1f
Nir Izraeli <nir@sentinel-labs.com>
2016-07-19 15:24:58 +0300

    Add pypi information
    Signed-off-by: Nir Izraeli <nir@sentinel-labs.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'timewatch',
+  packages = ['timewatch'], # this must be the same as the name above
+  version = '0.2',
+  description = 'A library automating worktime reports for timewatch.co.il',
+  author = 'Nir Izraeli',
+  author_email = 'nirizr@gmail.com',
+  url = 'https://github.com/nirizr/timewatch', # use the URL to the github repo
+  download_url = 'https://github.com/nirizr/timewatch/tarball/0.2', # I'll explain this in a second
+  keywords = ['timewatch', 'timewatch.co.il'], # arbitrary keywords
+  classifiers = [],
+)
