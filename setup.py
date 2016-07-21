55fd8048ad6c4e7395d6275f6f906ceedd6bfc25
Nir Izraeli <nir@sentinel-labs.com>
2016-07-21 17:26:10 +0300

    Now an includable package
    Signed-off-by: Nir Izraeli <nir@sentinel-labs.com>

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open('requirements.txt') as f:
 setup(
   name = 'timewatch',
   packages = ['timewatch'], # this must be the same as the name above
-  version = '0.5',
+  version = '0.5.1',
   description = 'A library automating worktime reports for timewatch.co.il',
   author = 'Nir Izraeli',
   author_email = 'nirizr@gmail.com',
   url = 'https://github.com/nirizr/timewatch', # use the URL to the github repo
-  download_url = 'https://github.com/nirizr/timewatch/tarball/0.5', # I'll explain this in a second
+  download_url = 'https://github.com/nirizr/timewatch/tarball/0.5.1', # I'll explain this in a second
   keywords = ['timewatch', 'timewatch.co.il'], # arbitrary keywords
   install_requires = requirements,
   classifiers = [],
