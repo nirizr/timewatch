4cd84112857eb83a04924507b7874ee658bfc4a1
Nir Izraeli <nirizr@gmail.com>
2016-07-25 00:09:29 +0300

    fix setup.py
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
+from os.path import exists
 from distutils.core import setup
-
-with open('requirements.txt') as f:
-  requirements = f.read().splitlines()
+import timewatch
 
 setup(
   name = 'timewatch',
-  packages = ['timewatch'], # this must be the same as the name above
-  version = '0.5.1',
+  packages = ['timewatch'],
+  version = timewatch.__version__,
   description = 'A library automating worktime reports for timewatch.co.il',
+  long_description=(open('README.md').read() if exists('README.md') else ''),
   author = 'Nir Izraeli',
   author_email = 'nirizr@gmail.com',
-  url = 'https://github.com/nirizr/timewatch', # use the URL to the github repo
-  download_url = 'https://github.com/nirizr/timewatch/tarball/0.5.1', # I'll explain this in a second
+  url = 'https://github.com/nirizr/timewatch',
+  download_url = 'https://github.com/nirizr/timewatch/tarball/{}'.format(timewatch.__version__),
   keywords = ['timewatch', 'timewatch.co.il'], # arbitrary keywords
-  install_requires = requirements,
   classifiers = [],
 )
